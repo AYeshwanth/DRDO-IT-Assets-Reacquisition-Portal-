91ad9697587ab46d135f00876b745ee69341b54d
AYeshwanth <akurathiyeshwanth@gmail.com>
2016-06-14 22:56:42 +0530

    mysql_fetch_array() problem debugged

--- connect.php
+++ connect.php
@@ -1,42 +1,35 @@
 <?php
 $inuser =$_POST["user"];
 $inpass =$_POST['pass'];
-$user="root";
-$password="";
-$db="temp";
+
 mysql_connect("localhost","root","");
 
-mysql_select_db($db)or ("Database not foyunf");
+mysql_select_db("test")or ("Database not foyunf");
 
-$query="SELECT * FROM 'user_1' WHERE 'name'='$inuser'";
-$querypass="SELECT * FROM 'user_1' WHERE 'pass'='$inpass'";
+$query="SELECT * FROM details WHERE first_name='$inuser'";
+$querypass="SELECT * FROM details WHERE second_name='$inpass'";
 
 $result= mysql_query($query);
 $resultpass=mysql_query($querypass);
 
-while ($row = mysql_fetch_assoc($result)) {
-    echo $row['user'];
-    echo $row['password'];
-}
 $row= mysql_fetch_array($result);
 $rowpass=mysql_fetch_array($resultpass);
 
-$serveruser = $row['user'];
-$serverpass = $row['pass'];
+$serveruser = $row['first_name'];
+$serverpass = $row['second_name'];
 
 if($serverpass&&$serveruser)
 {
-	if(!result)
+	if(!$result)
 	{
 		die("Username or password is invalid");
 	}
-	echo "Database Output";
-	mysql.close();
-	echo $inpass;
-	echo $serverpass;
+	//echo "Database Output";
+	//echo $inpass;
+	//echo $serverpass;
 }
 if($inpass==$serverpass)
-{
-	header('Location : home.html');
+{//echo 12;
+	header('Location:home.html');
 }
 ?>
